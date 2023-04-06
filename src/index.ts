93b0226888b9544d2634c836d9c410974680946a
Kacper Faber <kacperf1234@gmail.com>
2023-04-06 18:21:46 +0200

    Created index.ts file with just comment...

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+/**
+ * We have some work TODO...
+ * - Configure index.ts to generate automatically.
+ * - In some places my naming conventions are broken [Colours.white and Pieces.WhiteQueen].
+ */
