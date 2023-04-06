6d636c1fad9de17a4b9df81720bfeb4328525637
Kacper Faber <kacperf1234@gmail.com>
2023-04-06 13:59:17 +0200

    Created Colour and Colours [White and Black]. To Piece added some functions to play with colours and check that piece is empty [async]

---
+++ src/colour.ts
@@ -0,0 +1,11 @@
+export type Colour = "white" | "black";
+
+interface IColours {
+    White: Colour;
+    Black: Colour;
+}
+
+export const Colours: IColours = {
+    White: "white",
+    Black: "black"
+}
--- src/piece.ts
+++ src/piece.ts
@@ -1,4 +1,5 @@
 import Pieces from "./pieces";
+import {Colour, Colours} from "./colour";
 
 export type Piece =
     typeof Pieces.BlackKing |
@@ -14,3 +15,53 @@ export type Piece =
     typeof Pieces.WhiteRook |
     typeof Pieces.BlackRook |
     typeof Pieces.Empty;
+
+const AllWhite = [
+    Pieces.WhiteRook,
+    Pieces.WhiteQueen,
+    Pieces.WhiteKing,
+    Pieces.WhiteBishop,
+    Pieces.WhiteKnight,
+    Pieces.WhitePawn
+];
+
+const AllBlack = [
+    Pieces.BlackRook,
+    Pieces.BlackQueen,
+    Pieces.BlackKing,
+    Pieces.BlackBishop,
+    Pieces.BlackKnight,
+    Pieces.BlackPawn
+];
+
+interface IPiece {
+    getColour(piece: Piece): Promise<Colour | null>;
+    isWhite(piece: Piece): Promise<boolean>;
+    isBlack(piece: Piece): Promise<boolean>;
+    isEmpty(piece: Piece): Promise<boolean>;
+    isColour(piece: Piece, colour: Colour): Promise<boolean>;
+}
+
+export const Piece: IPiece = {
+    async getColour(piece: Piece): Promise<Colour | null> {
+        if (piece in AllBlack) return Colours.White;
+        else if (piece in AllBlack) return Colours.Black;
+        return null;
+    },
+    
+    async isWhite(piece: Piece): Promise<boolean> {
+        return piece in AllWhite;
+    },
+
+    async isBlack(piece: Piece): Promise<boolean> {
+        return piece in AllBlack;
+    },
+
+    async isEmpty(piece: Piece): Promise<boolean> {
+        return piece == Pieces.Empty;
+    },
+
+    async isColour(piece: Piece, colour: Colour): Promise<boolean> {
+        return await this.getColour(piece) == colour;
+    }
+}
