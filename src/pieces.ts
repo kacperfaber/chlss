587a6c2cb06d27627d28376be771c02afbbea8c2
Kacper Faber <kacperf1234@gmail.com>
2023-04-06 11:16:34 +0200

    Created file pieces.ts with definition of export Pieces { WhiteRook: string... }

---
+++ src/pieces.ts
@@ -0,0 +1,31 @@
+interface IPieces {
+    WhiteRook: string;
+    WhiteKing: string,
+    WhiteQueen: string,
+    WhitePawn: string,
+    WhiteBishop: string,
+    WhiteKnight: string,
+    BlackRook: string,
+    BlackKing: string,
+    BlackQueen: string,
+    BlackPawn: string,
+    BlackBishop: string,
+    BlackKnight: string
+}
+
+const Pieces: IPieces = {
+    WhiteRook: 'R',
+    WhiteKing: 'K',
+    WhiteQueen: 'Q',
+    WhitePawn: 'P',
+    WhiteBishop: 'B',
+    WhiteKnight: 'N',
+    BlackRook: 'r',
+    BlackKing: 'k',
+    BlackQueen: 'q',
+    BlackPawn: 'p',
+    BlackBishop: 'b',
+    BlackKnight: 'n'
+}
+
+export default Pieces;
