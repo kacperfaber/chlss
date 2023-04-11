f8ce3c2e227bb20c778ab50e8b41a6b5aca6cebe
Kacper Faber <kacperf1234@gmail.com>
2023-04-11 12:52:37 +0200

    added test boardPosition.copyAsync.test.ts

--- test/boardPosition.copyAsync.test.ts
+++ test/boardPosition.copyAsync.test.ts
@@ -18,6 +18,16 @@ describe('boardPosition.ts', function () {
             }
         });
 
+        test(`returns expected data - not empty board`, async function() {
+            const board = await BoardPosition.createDefault();
+            const copy = await BoardPosition.copyAsync(board);
+
+            for (let i = 0; i < 64; i++) {
+                let index = i as SquareIndex;
+                expect(await BoardPosition.getPiece(board, index)).toBe(await BoardPosition.getPiece(copy, index));
+            }
+        });
+
         test(`returns not a original board`, async function() {
             const board = await BoardPosition.createEmpty();
             const copy = await BoardPosition.copyAsync(board);
