3f3dae4efb71e448834e5a03acd05c47f87ce3f4
Kacper Faber <kacperf1234@gmail.com>
2023-04-14 13:44:12 +0200

    move.ts - added comment.
    To move.ts added comment to describe the move implementation [I have 4 properties and I have to know what means]

--- src/move.ts
+++ src/move.ts
@@ -1,6 +1,22 @@
 import {SquareIndex} from "./square";
 import {Piece} from "./piece";
 
+
+/*
+Move implementation:
+
+1. Castling - TODO:
+
+2. En Passant:
+'targetPiece' is set to enemy pawn, but the 'to' square is empty. Then we have to calculate the pawn
+position to take.
+
+3. Other situations:
+We can just replace 'piece' with piece on the 'to', and set 'from' square empty.
+
+ */
+
+
 export interface IMove {
     from: SquareIndex;
     to: SquareIndex;
