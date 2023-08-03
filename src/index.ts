62259ce398b0e134a215d1bc56bb9e4adafcdb87
Kacper Faber <kacperf1234@gmail.com>
2023-08-03 18:08:23 +0200

    Prepare for publish
    index.ts: exports some things like BoardObj, UCI, FEN and something more.
    
    package.json - fixed $.main and add 'files', 'types'
    
    tsconfig.json - set declaration: true.

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,15 @@
  * - In some places my naming conventions are broken [Colours.white and Pieces.WhiteQueen].
  * - Created something like 'Line Generator', but using {x,y} offsets [we can use it in Knight and King]
  */
+
+import Pieces from "./pieces";
+
+export {BoardObj} from "./boardObj";
+export {UCI} from "./uci";
+export {FEN} from "./fen";
+export {BoardNotation} from "./boardNotation";
+export {Coords} from "./coords";
+export {Pieces};
+export {Piece} from "./piece"
+export {Colour} from "./colour"
+export {IMove} from "./move"
